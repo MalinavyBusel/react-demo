67dd27177a83a9cfe6562e5a666cad3be23edb2c
MalinavyBusel <egorbgtrvch@gmail.com>
2024-08-09 17:14:50 +0300

    added password visibility toggling

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import { useForm, FieldValues } from 'react-hook-form';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import authorize from '../../helpers/api/login';
 import './LoginForm.css';
 
@@ -11,6 +11,14 @@ function LoginForm() {
   const navigate = useNavigate();
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const authorizeHandler = (formBody: FieldValues) => authorize(formBody, navigate, setErrorMsg);
+  const toggleVisibility = () => {
+    const visibilityButton = document.getElementsByClassName('password-visibility-button')[0];
+    const passwordField = document.getElementsByClassName('password-input')[0];
+    visibilityButton.classList.toggle('unseen');
+    const inputType = passwordField.attributes.getNamedItem('type');
+    const newTypeValue = inputType!.value === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', newTypeValue);
+  };
 
   return (
     <div className="form-container">
@@ -18,9 +26,12 @@ function LoginForm() {
         <h2 className="form-header">Login</h2>
       </div>
       <form className="login-form" onSubmit={handleSubmit(authorizeHandler)}>
-        <input type="email" className="input-with-text" {...register('email')} placeholder="email" />
-        <FontAwesomeIcon icon={faEye} />
-        <input type="password" className="input-with-text" {...register('password')} placeholder="password" />
+        <input type="email" className="input-with-text unseen" {...register('email')} placeholder="email" />
+        <div className="password-visibility-button unseen" onClick={toggleVisibility}>
+          <FontAwesomeIcon icon={faEye} className="eye" />
+          <FontAwesomeIcon icon={faEyeSlash} className="eye-slash" />
+        </div>
+        <input type="password" className="input-with-text password-input" {...register('password')} placeholder="password" />
         {errorMsg !== null && <div className="error-container">{errorMsg}</div>}
         <button type="submit" className="submit-button">Enter</button>
       </form>
